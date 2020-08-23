37a443570ee589a742db253928fe0a6d8815f7f8
Prashasti Gupta <princess.prashastigupta@gmail.com>
2020-08-23 22:35:17 +0530

    Updates

---
+++ precache-manifest.fd73d48e1276f3e111f0fa4366468ccd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9276af45b38bef5ab7555cdc176642",
+    "url": "/ToDoApp-React-build/index.html"
+  },
+  {
+    "revision": "1cde1d1ddba19731255b",
+    "url": "/ToDoApp-React-build/static/css/main.f27482e7.chunk.css"
+  },
+  {
+    "revision": "6c6defece35a365f511a",
+    "url": "/ToDoApp-React-build/static/js/2.d7e71e85.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/ToDoApp-React-build/static/js/2.d7e71e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cde1d1ddba19731255b",
+    "url": "/ToDoApp-React-build/static/js/main.fabcaa45.chunk.js"
+  },
+  {
+    "revision": "631977f5d3a7d7ccf77b",
+    "url": "/ToDoApp-React-build/static/js/runtime-main.03183dd8.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ToDoApp-React-build/static/media/logo.ee7cd8ed.svg"
+  }
+]);
